03382b9a21be21aa160ad9a7927458e463f6cbed
golfstrimmar <yushinbox@gmail.com>
2025-02-26 10:02:59 +0100

    refresh

--- backend/server.js
+++ backend/server.js
@@ -699,7 +699,7 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello from the WebSocket server 5000!</h1>");
 });
 
-// Запуск сервера
-server.listen(5000, () => {
-  console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000; // По умолчанию 5000, если PORT не задан
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
